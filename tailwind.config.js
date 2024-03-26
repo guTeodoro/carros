6124334037dad82c38ee1db89fe08011beb83a58
Gustavo Teodoro <gustavoteodsant@gmail.com>
2024-03-26 19:48:50 -0300

    sexto commit do App

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
       'dark-green': '#004140',
       'offwhite': 'rgba(255, 255, 255, 0.75',
       'white': '#fff',
-      'offwhitebg': 'f2f2f2',
+      'offwhitebg': '#f2f2f2',
     },
     fontFamily:{
       'shoulders': '"Big Shoulders Display"',
