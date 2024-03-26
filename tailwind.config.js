64319b6936cb67ffe3e4b4118d291fc55815029c
Gustavo Teodoro <gustavoteodsant@gmail.com>
2024-03-26 19:17:58 -0300

    quarto commit do App

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ export default {
       'offwhitebg': 'f2f2f2',
     },
     fontFamily:{
-      'shouders': '"Big Shoulders Display"',
-      'lexend': '"Lexend Deca"',
+      'shoulders': '"Big Shoulders Display"',
+      'lexend': '"Lexend Deca"'
     }
   },
   plugins: [],
